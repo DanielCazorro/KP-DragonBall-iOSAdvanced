a795e46ff0fbd99c6fab702877fe5747e1d23387
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-23 20:52:55 +0200

    Innitial Commit: Basic Structure

---
+++ KP-DragonBall-iOSAdvanced/KP-DragonBall-iOSAdvanced/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KP-DragonBall-iOSAdvanced
+//
+//  Created by Daniel Cazorro Frías on 23/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
