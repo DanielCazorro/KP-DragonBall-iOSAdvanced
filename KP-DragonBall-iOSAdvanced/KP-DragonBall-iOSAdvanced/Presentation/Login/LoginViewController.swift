37ccaccd9641ff7fcda6cd301be96b841bdc61b1
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-24 17:24:45 +0200

    Basic show keyboard

--- KP-DragonBall-iOSAdvanced/KP-DragonBall-iOSAdvanced/Presentation/Login/LoginViewController.swift
+++ KP-DragonBall-iOSAdvanced/KP-DragonBall-iOSAdvanced/Presentation/Login/LoginViewController.swift
@@ -13,23 +13,66 @@ protocol LoginViewControllerDelegate {
 
 class LoginViewController: UIViewController {
 
+    // MARK: - IBOutlets -
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var emailFieldError: UILabel!
     @IBOutlet weak var passwordFieldError: UILabel!
     @IBOutlet weak var loadingView: UIView!
     
+    // MARK: - IBActions -
     @IBAction func onLoginPressed() {
         viewModel?.onLoginPressed(
             email: emailField.text,
             password: passwordField.text)
     }
     
+    // MARK: - Public Properties -
     var viewModel: LoginViewControllerDelegate?
     
+    private enum FieldType: Int {
+        case email = 0
+        case password
+    }
+    
+    // MARK: - Lifecycle -
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initViews()
     }
+    
+    // MARK: - Private functions -
+    private func initViews() {
+        emailField.delegate = self
+        emailField.tag = FieldType.email.rawValue
+        passwordField.delegate = self
+        passwordField.tag = FieldType.email.rawValue
+        
+        // Oculta el teclado al pulsar la pantalla
+        view.addGestureRecognizer(
+            UITapGestureRecognizer(
+                target: self,
+                action: #selector(dismissKeyboard)
+            )
+        )
+    }
+    
+    @objc func dismissKeyboard() {
+        
+    }
+}
 
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        switch FieldType(rawValue: textField.tag) {
+        case .email:
+            emailFieldError.isHidden = true
+            
+        case .password:
+            passwordFieldError.isHidden = true
+            
+        default:
+            break
+        }
+    }
 }
