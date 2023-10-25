1fd7b4259bbd726caf4be51c0d5a877d2050ac2f
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-25 21:39:40 +0200

    Basic heroes view

--- KP-DragonBall-iOSAdvanced/KP-DragonBall-iOSAdvanced/Presentation/Heroes/HeroesViewController.swift
+++ KP-DragonBall-iOSAdvanced/KP-DragonBall-iOSAdvanced/Presentation/Heroes/HeroesViewController.swift
@@ -4,10 +4,31 @@
 //
 //  Created by Daniel Cazorro Frías on 25/10/23.
 //
+import UIKit
 
 class HeroesViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initViews()
+    }
+    
+    private func initViews() {
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 }
+
+extension HeroesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+  
+}
+
