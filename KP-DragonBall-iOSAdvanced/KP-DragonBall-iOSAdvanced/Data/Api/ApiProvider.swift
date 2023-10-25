dbde1fe2ee66dda8a4dc40ad7dbf8f804dd0917e
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-25 10:03:00 +0200

    ApiProvider

---
+++ KP-DragonBall-iOSAdvanced/KP-DragonBall-iOSAdvanced/Data/Api/ApiProvider.swift
@@ -0,0 +1,13 @@
+//
+//  ApiProvider.swift
+//  KP-DragonBall-iOSAdvanced
+//
+//  Created by Daniel Cazorro Frías on 25/10/23.
+//
+
+import Foundation
+
+
+class ApiProvider {
+    static private let apiBaseURL = "https://dragonball.keepcoding.education/api"
+}
