f414030e22635a29b4d8bd9d17d0a944ed974da8
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-25 11:12:04 +0200

    Basic Login Api

--- KP-DragonBall-iOSAdvanced/KP-DragonBall-iOSAdvanced/Data/Api/ApiProvider.swift
+++ KP-DragonBall-iOSAdvanced/KP-DragonBall-iOSAdvanced/Data/Api/ApiProvider.swift
@@ -7,7 +7,61 @@
 
 import Foundation
 
-
-class ApiProvider {
-    static private let apiBaseURL = "https://dragonball.keepcoding.education/api"
+extension NotificationCenter {
+    static let apiLoginNotification = Notification.Name("NOTIFICATION_API_LOGIN")
+    static let tokenKey = "KEY_TOKEN"
 }
+
+protocol ApiProviderProtocol {
+    func login(for user: String, with password: String)
+}
+
+class ApiProvider: ApiProviderProtocol {
+    // MARK: - Constants -
+    static private let apiBaseURL = "https://dragonball.keepcoding.education/api"
+    
+    private enum Endpoint {
+        static let login = "/auth/login"
+    }
+    
+    // MARK: - ApiProviderProtocol -
+    func login(for user: String, with password: String) {
+        guard let url = URL(string: "\(ApiProvider.apiBaseURL)\(Endpoint.login)") else {
+            return
+        }
+        
+        guard let loginData = String(format: "%@:%@",
+                                     user, password).data(using: .utf8)?.base64EncodedString() else {
+            return
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.setValue("Basic \(loginData)",
+                            forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            guard error == nil else {
+                // TODO: Enviar notificación indicando el error
+                return
+            }
+            
+            guard let data,
+                  (response as? HTTPURLResponse)?.statusCode == 200 else {
+                // TODO: Enviar notificación indicando response error
+                return
+            }
+            
+            guard let responseData = String(data: data, encoding: .utf8) else {
+                // TODO: enviar notificación indicando response vacío
+                return
+            }
+            
+            NotificationCenter.default.post(
+                name: NotificationCenter.apiLoginNotification,
+                object: nil,
+                userInfo: [NotificationCenter.tokenKey: responseData])
+        } .resume()
+    }
+}
+
