f74d5c6d80608caee42663db4f1c0fae7347b5a3
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-25 21:57:52 +0200

    TableViewCell Heroes

--- KP-DragonBall-iOSAdvanced/KP-DragonBall-iOSAdvanced/Presentation/Heroes/Cell/HeroCellView.swift
+++ KP-DragonBall-iOSAdvanced/KP-DragonBall-iOSAdvanced/Presentation/Heroes/Cell/HeroCellView.swift
@@ -5,4 +5,17 @@
 //  Created by Daniel Cazorro Frías on 25/10/23.
 //
 
-import Foundation
+import UIKit
+
+class HeroCellView: UITableViewCell {
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+    }
+    
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
