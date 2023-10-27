2e8fed0744f37b96b50001e17a400f8fb15e4bb7
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-27 11:21:45 +0200

    Remove core data from appdelegate

---
+++ KP-DragonBall-iOSAdvanced/KP-DragonBall-iOSAdvanced/Data/Local/Models/CoreDataStack.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataStack.swift
+//  KP-DragonBall-iOSAdvanced
+//
+//  Created by Daniel Cazorro Frías on 26/10/23.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack: NSObject {
+    
+}
