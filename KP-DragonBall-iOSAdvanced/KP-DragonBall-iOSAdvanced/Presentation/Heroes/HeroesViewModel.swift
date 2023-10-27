9957f5acd03b171c070ce7914d7347ccb2b68e45
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-27 13:19:18 +0200

    ViewModel updating

--- KP-DragonBall-iOSAdvanced/KP-DragonBall-iOSAdvanced/Presentation/Heroes/HeroesViewModel.swift
+++ KP-DragonBall-iOSAdvanced/KP-DragonBall-iOSAdvanced/Presentation/Heroes/HeroesViewModel.swift
@@ -1,19 +1,23 @@
 //
 //  HeroesViewModel.swift
-//  KP-DragonBall-iOSAdvanced
+//  DragonBall
 //
 //  Created by Daniel Cazorro Frías on 25/10/23.
 //
 
 import Foundation
 
-class HeroesViewModel {
-   
+class HeroesViewModel: HeroesViewControllerDelegate {
+    
     // MARK: - Dependencies-
     private let apiProvider: ApiProviderProtocol
     private let secureDataProvider: SecureDataProviderProtocol
     
     // MARK: - Properties -
+    var viewState: ((HeroesViewState) -> Void)?
+    var heroesCount: Int {
+        heroes.count
+    }
     private var heroes: Heroes = []
     
     //MARK: - Initializers -
@@ -24,13 +28,20 @@ class HeroesViewModel {
     }
     
     func onViewAppear() {
+        viewState?(.loading(true))
         DispatchQueue.global().async {
+            defer { self.viewState?(.loading(false))}
             guard let token = self.secureDataProvider.getToken() else {return}
             self.apiProvider.getHeroes(by: nil,
                                        token: token) { heroes in
-                
+                self.heroes = heroes
+                self.viewState?(.updateData)
             }
         }
+    }
+    
+    func heroBy(index: Int) -> Hero? {
+        heroes[index]
     }
     
 }
