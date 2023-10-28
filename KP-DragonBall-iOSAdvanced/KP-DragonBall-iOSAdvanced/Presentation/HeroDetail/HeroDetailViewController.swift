d6e6aa06d3f8fafec10ddc2e219210d5ffd13400
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-28 10:03:16 +0200

    Innitial struct viewModel

--- KP-DragonBall-iOSAdvanced/KP-DragonBall-iOSAdvanced/Presentation/HeroDetail/HeroDetailViewController.swift
+++ KP-DragonBall-iOSAdvanced/KP-DragonBall-iOSAdvanced/Presentation/HeroDetail/HeroDetailViewController.swift
@@ -1,6 +1,6 @@
 //
 //  HeroDetailViewController.swift
-//  KP-DragonBall-iOSAdvanced
+//  DragonBall
 //
 //  Created by Daniel Cazorro Frías on 28/10/23.
 //
@@ -8,14 +8,47 @@
 import UIKit
 import MapKit
 
+protocol HeroDetailViewControllerDelegate {
+    var viewState: ((HeroDetailViewState) -> Void)? {get set}
+    
+    func onViewAppear()
+}
+
+enum HeroDetailViewState {
+    case loading(_ isLoading: Bool)
+    case update(hero: Hero?, locations: [String])
+}
+
 class HeroDetailViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var photo: UIImageView!
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var heroDescription: UITextView!
     
+    var viewModel: HeroDetailViewControllerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initViews()
+        setObservers()
+        viewModel?.onViewAppear()
+    }
+    
+    private func initViews() {
+        
+    }
+    
+    private func setObservers() {
+        viewModel?.viewState = { [weak self] state in
+            DispatchQueue.main.async {
+                switch state {
+                case .loading(let isLoading):
+                    break
+                    
+                case .update(let hero, let locations):
+                    break
+                }
+            }
+        }
     }
 }
--- KP-DragonBall-iOSAdvanced/KP-DragonBall-iOSAdvanced/Presentation/HeroDetail/HeroDetailViewModel.swift
+++ KP-DragonBall-iOSAdvanced/KP-DragonBall-iOSAdvanced/Presentation/HeroDetail/HeroDetailViewModel.swift
@@ -1,12 +1,28 @@
 //
 //  HeroDetailViewModel.swift
-//  KP-DragonBall-iOSAdvanced
+//  DragonBall
 //
 //  Created by Daniel Cazorro Frías on 28/10/23.
 //
 
 import UIKit
 
-class HeroDetailViewModel {
+class HeroDetailViewModel: HeroDetailViewControllerDelegate {
+    private let apiProvider: ApiProviderProtocol
+    
+    var viewState: ((HeroDetailViewState) -> Void)?
+    
+    init(apiProvider: ApiProviderProtocol) {
+        self.apiProvider = apiProvider
+    }
+    
+    func onViewAppear() {
+        viewState?(.loading(true))
+        
+        DispatchQueue.global().async {
+            
+        }
+    }
+    
     
 }
