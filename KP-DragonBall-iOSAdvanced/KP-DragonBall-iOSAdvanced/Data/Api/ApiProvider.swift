0f659653990f386748d6fceb01b2dfdba507e803
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-28 10:10:03 +0200

    ApiProvider: GetLocations func

--- KP-DragonBall-iOSAdvanced/KP-DragonBall-iOSAdvanced/Data/Api/ApiProvider.swift
+++ KP-DragonBall-iOSAdvanced/KP-DragonBall-iOSAdvanced/Data/Api/ApiProvider.swift
@@ -15,6 +15,7 @@ extension NotificationCenter {
 protocol ApiProviderProtocol {
     func login(for user: String, with password: String)
     func getHeroes(by name: String?, token: String, completion: ((Heroes) -> Void)?)
+    func getLocations(by heroId: String?, token: String, completion: ((HeroLocations) -> Void)?)
 }
 
 class ApiProvider: ApiProviderProtocol {
@@ -109,6 +110,48 @@ class ApiProvider: ApiProviderProtocol {
             
             print("API RESPONSE - GET HEROES: \(heroes)")
             completion?(heroes)
+        }.resume()
+    }
+    
+    func getLocations(by heroId: String?, token: String, completion: ((HeroLocations) -> Void)?) {
+        guard let url = URL(string: "\(ApiProvider.apiBaseURL)\(Endpoint.heroLocations)") else {
+            // TODO: Enviar notificación indicando el error
+            return
+        }
+        
+        let jsonData: [String: Any] = ["id": heroId ?? ""]
+        let jsonParameters = try? JSONSerialization.data(withJSONObject: jsonData)
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.setValue("application/json; charset=utf-8",
+                            forHTTPHeaderField: "Content-Type")
+        urlRequest.setValue("Bearer \(token)",
+                            forHTTPHeaderField: "Authorization")
+        urlRequest.httpBody = jsonParameters
+        
+        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            guard error == nil else {
+                // TODO: Enviar notificación indicando el error
+                completion?([])
+                return
+            }
+            
+            guard let data,
+                  (response as? HTTPURLResponse)?.statusCode == 200 else {
+                // TODO: Enviar notificación indicando response error
+                completion?([])
+                return
+            }
+            
+            guard let heroLocations = try? JSONDecoder().decode(HeroLocations.self, from: data) else {
+                // TODO: Enviar notificación indicando response error
+                completion?([])
+                return
+            }
+            
+            print("API RESPONSE - GET HERO LOCATIONS: \(heroLocations)")
+            completion?(heroLocations)
         }.resume()
     }
 }
